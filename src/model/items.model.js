d6a37fa98228ffed5cd3d7485ad66de2065e7abe
alexanderZaporozhtsev <randallyourmaker@gmail.com>
2024-09-27 14:50:21 +0300

    add methods for cart logic

--- src/model/items.model.js
+++ src/model/items.model.js
@@ -1,17 +1,61 @@
 export function createItemsModel() {
   return {
     items: [],
+    cart: [],
 
     setItems: function (items) {
-      this.items = [];
-
       items.forEach((item) => {
         this.items.push(item);
       });
     },
 
+    setCart: function (cartItems) {
+      this.cart = cartItems;
+    },
+
+    getCartItemsImg: function () {
+      const cartItemsImgs = [];
+
+      this.items.forEach((item) => {
+        this.cart.forEach((cartItem) => {
+          if (item.id == cartItem.id) cartItemsImgs.push(item.img);
+        });
+      });
+
+      return cartItemsImgs;
+    },
+
     getMainItems: function () {
       return this.items;
+    },
+
+    getCartItems() {
+      return this.cart;
+    },
+
+    addToCart: function (id) {
+      const cartItemToFind = this.cart.find((cartItem) => cartItem.id == id);
+
+      if (cartItemToFind) {
+        cartItemToFind.amount += 1;
+      } else {
+        this.cart.push({
+          id: id,
+          amount: 1,
+        });
+      }
+
+      console.log(this.cart);
+    },
+
+    getItemById: function (id) {
+      let itemById = {};
+
+      this.items.forEach((item) => {
+        if (item.id === parseInt(id)) itemById = item;
+      });
+
+      return itemById;
     },
   };
 }
