f0457b8f5253dd4277a93dd9d5aa8f59831905f5
alexanderZaporozhtsev <randallyourmaker@gmail.com>
2024-10-01 09:28:31 +0300

    refactoring. decomposing the code

--- src/model/items.model.js
+++ src/model/items.model.js
@@ -1,7 +1,6 @@
-export function createItemsModel() {
+export function createModelItems() {
   return {
     items: [],
-    cart: [],
 
     setItems: function (items) {
       items.forEach((item) => {
@@ -9,53 +8,8 @@ export function createItemsModel() {
       });
     },
 
-    setCart: function (cartItems) {
-      this.cart = cartItems;
-    },
-
-    getCartItemsImg: function () {
-      const cartItemsImgs = [];
-
-      this.items.forEach((item) => {
-        this.cart.forEach((cartItem) => {
-          if (item.id == cartItem.id) cartItemsImgs.push(item.img);
-        });
-      });
-
-      return cartItemsImgs;
-    },
-
-    getMainItems: function () {
+    getItems: function () {
       return this.items;
-    },
-
-    getCartItems() {
-      return this.cart;
-    },
-
-    addToCart: function (id) {
-      const cartItemToFind = this.cart.find((cartItem) => cartItem.id == id);
-
-      if (cartItemToFind) {
-        cartItemToFind.amount += 1;
-      } else {
-        this.cart.push({
-          id: id,
-          amount: 1,
-        });
-      }
-
-      console.log(this.cart);
-    },
-
-    getItemById: function (id) {
-      let itemById = {};
-
-      this.items.forEach((item) => {
-        if (item.id === parseInt(id)) itemById = item;
-      });
-
-      return itemById;
     },
   };
 }
