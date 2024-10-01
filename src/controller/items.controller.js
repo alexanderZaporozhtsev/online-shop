ba633b90a694dfc3d12b3e455e5da1d03fba3e7d
alexanderZaporozhtsev <randallyourmaker@gmail.com>
2024-10-01 09:29:13 +0300

    refactoring

--- src/controller/items.controller.js
+++ src/controller/items.controller.js
@@ -1,24 +1,38 @@
 import { createItemsStorage } from "../storage";
-import { createItemsModel } from "../model/items.model";
-import { createItemsView } from "../view/items.view";
+import { createModelItems } from "../model/items.model";
+import { createViewItems } from "../view/items.view";
 import { createLocalStorage } from "../localStorage";
+import { createViewCart } from "../view/cart-preview.view";
+import { createModelCart } from "../model/cart.model";
+import { createModelUser } from "../model/user.model";
 
 const storage = createItemsStorage("items");
-const model = createItemsModel();
-const view = createItemsView(handleClickAddToCart);
 const localStorage = createLocalStorage();
 
+const modelItems = createModelItems();
+const modelCart = createModelCart();
+
+const viewItems = createViewItems(handleClickAddToCart);
+const viewCartPreview = createViewCart();
+
+const modelUser = createModelUser();
+
 localStorage.userInit();
+modelUser.setUser(localStorage.getUserId());
 
 storage.read().then((items) => {
-  model.setItems(items);
-  model.setCart(localStorage.read());
-  view.renderMainItems(model.getMainItems());
-  view.renderCartItems(model.getCartItemsImg());
+  modelItems.setItems(items);
+  modelCart.setCart(localStorage.read());
+  viewItems.render(modelItems.getItems());
+  viewCartPreview.renderCartItems(
+    modelCart.getCartItemsImg(modelItems.getItems())
+  );
 });
 
 function handleClickAddToCart(id) {
-  model.addToCart(id);
-  localStorage.create(model.getCartItems());
-  view.addToCart(model.getItemById(id));
+  modelCart.addToCart(id);
+  localStorage.create(modelCart.getCartItems());
+  viewCartPreview.addToCart(modelCart.getItemById(id));
 }
+
+console.log(modelUser.user);
