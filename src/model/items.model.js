c4f50ecafa0d79e3765c63d76787de2b5370c85e
alexanderZaporozhtsev <randallyourmaker@gmail.com>
2024-10-02 16:22:30 +0300

    new method getItemById

--- src/model/items.model.js
+++ src/model/items.model.js
@@ -11,5 +11,15 @@ export function createModelItems() {
     getItems: function () {
       return this.items;
     },
+
+    getItemById: function (id) {
+      let itemById = {};
+
+      this.items.forEach((item) => {
+        if (item.id === parseInt(id)) itemById = item;
+      });
+
+      return itemById;
+    },
   };
 }
