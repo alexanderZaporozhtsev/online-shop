cde4927eb9af44b40b0fab00b18f7802d87b457e
alexanderZaporozhtsev <randallyourmaker@gmail.com>
2024-10-11 10:38:08 +0300

    new event listeners and method to update some information

--- src/view/checkout.view.js
+++ src/view/checkout.view.js
@@ -1,18 +1,29 @@
-export function createViewCheckout(onAddressInfoChange) {
+export function createViewCheckout(
+  onAddressInfoChange,
+  onPaymentMethodChange,
+  onNewOrderAdd
+) {
   const checkoutNode = document.querySelector(".cart-item-output");
   const sumNode = document.querySelector(".checkout-pricing-items__amount");
   const fullSumNode = document.querySelector(".checkout-pricing__amount");
+  const addressInfoNode = document.querySelector(
+    ".checkout-address-info-container"
+  );
 
   const nameInput = document.getElementById("name");
   const addressInput = document.getElementById("address");
   const cityInput = document.getElementById("city");
   const numberInput = document.getElementById("number");
 
+  const cashCheckbox = document.getElementById("cash");
+  const cardCheckbox = document.getElementById("card");
+
   const changeAddressBtn = document.querySelector(".check-out-info-edit-btn");
   const closeChangeAddressBtn = document.querySelector(
     ".btn-close-address-popup"
   );
   const submitAddressBtn = document.querySelector(".btn-submit-address");
+  const checkoutAddBtn = document.querySelector(".checkout-btn");
 
   return {
     render: function (items) {
@@ -59,7 +70,35 @@ export function createViewCheckout(onAddressInfoChange) {
         };
 
         onAddressInfoChange(newAddressInfo);
+        this.updateAddressInfo(newAddressInfo);
         this.toggleChangeAddress();
+      });
+
+      cardCheckbox.addEventListener("click", () => {
+        if (!cardCheckbox.checked) {
+          cashCheckbox.checked = true;
+        } else {
+          cardCheckbox.checked = true;
+          cashCheckbox.checked = false;
+        }
+
+        onPaymentMethodChange();
+      });
+
+      cashCheckbox.addEventListener("click", () => {
+        if (!cashCheckbox.checked) {
+          cardCheckbox.checked = true;
+        } else {
+          cashCheckbox.checked = true;
+          cardCheckbox.checked = false;
+        }
+
+        onPaymentMethodChange();
+      });
+
+      checkoutAddBtn.addEventListener("click", () => {
+        onNewOrderAdd();
+        alert("заказ успешно добавлен");
       });
     },
 
@@ -71,6 +110,25 @@ export function createViewCheckout(onAddressInfoChange) {
     toggleChangeAddress: function () {
       const overlay = document.getElementById("popupOverlay");
       overlay.classList.toggle("show");
+    },
+
+    updateAddressInfo: function (newAddressInfo) {
+      addressInfoNode.innerHTML = "";
+
+      const addressInfoHTML = `
+        <p class="checkout-address-info__name">${newAddressInfo.addressName}</p>
+        <p class="checkout-address-info__address">
+          ${newAddressInfo.addressLine}
+        </p>
+        <p class="checkout-address-info__city">${newAddressInfo.addressCity}</p>
+        <p class="checkout-address-info__number">${newAddressInfo.addressNumber}</p>
+      `;
+
+      addressInfoNode.innerHTML = addressInfoHTML;
+    },
+
+    throwError: function () {
+      alert("Введенные данные некорректны");
     },
   };
 }
