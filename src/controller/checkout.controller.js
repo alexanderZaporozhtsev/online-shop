e7be85dc7e7100129d26a40b202bf75443b6fede
alexanderZaporozhtsev <randallyourmaker@gmail.com>
2024-10-11 10:35:17 +0300

    new handlers (change payment method, add new order)

--- src/controller/checkout.controller.js
+++ src/controller/checkout.controller.js
@@ -1,3 +1,4 @@
+import { isBoolean } from "util";
 import { createLocalStorage } from "../localStorage";
 import { createModelCart } from "../model/cart.model";
 import { createModelCheckout } from "../model/checkout.model";
@@ -12,7 +13,11 @@ const modelCart = createModelCart();
 const modelCheckout = createModelCheckout();
 const modelUser = createModelUser();
 
-const viewCheckout = createViewCheckout(handleAddressInfoChange);
+const viewCheckout = createViewCheckout(
+  handleAddressInfoChange,
+  handlePaymentMethodChange,
+  handleNewOrderAdd
+);
 
 storage.readById(localStorage.read()).then((items) => {
   modelUser.setUser(localStorage.getUserId());
@@ -26,4 +31,15 @@ function handleAddressInfoChange(newAddressInfo) {
   modelCheckout.setAddressInfo(newAddressInfo);
   modelCheckout.setUserId(modelUser.getUser());
   modelCheckout.setItems(modelCart.getCartItems());
+}
+
+function handlePaymentMethodChange() {
+  modelCheckout.switchPaymentMethod();
+  console.log(modelCheckout.checkout.paymentMethodIsCard);
+}
+
+function handleNewOrderAdd() {
+  storage.createOrder(modelCheckout.getCheckout());
+  localStorage.clearCart();
+  location.href = "/";
 }
